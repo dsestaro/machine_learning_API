e962af3a3d2e83bdf8184efd18f3e21479cd1637
Davidson Sestaro <dsestaro@gmail.com>
2017-01-16 22:47:03 +0000

    Creating class to incapsulate logic to verify if a String is valid (not null and not empty)

---
+++ src/main/java/br/com/algorithms/machine/learning/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package br.com.algorithms.machine.learning.utils;
+
+public class StringUtils {
+
+  public static boolean isEmpty(String value) {
+
+    return (value == null || value.length() == 0);
+  }
+}
---
+++ src/test/java/br/com/algorithms/machine/learning/utils/StringUtilsTests.java
@@ -0,0 +1,9 @@
+package br.com.algorithms.machine.learning.supervisioned.tree.utils;
+
+public class StringUtils {
+
+  public static boolean isEmpty(String value) {
+
+    return (value == null || value.length() == 0);
+  }
+}
