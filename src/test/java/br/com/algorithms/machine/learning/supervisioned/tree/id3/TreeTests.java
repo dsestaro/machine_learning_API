1532aa9f31105359a452d4ee326ae955c756d67c
Davidson Sestaro <dsestaro@gmail.com>
2017-02-14 16:34:21 +0000

    Finish the refactor of the class TreeTests

--- src/test/java/br/com/algorithms/machine/learning/supervisioned/tree/id3/TreeTests.java
+++ src/test/java/br/com/algorithms/machine/learning/supervisioned/tree/id3/TreeTests.java
@@ -17,6 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -27,153 +28,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 public class TreeTests {
-
-  private TreeImpl tree;
-  private Instances instances;
-  private Features features;
-
-  @Before
-  public void instanatiateTree() {
-
-    this.tree = new TreeImpl();
-
-    this.instances = new InstancesImpl();
-    this.features = new FeaturesImpl();
-
-    Feature featureOutlook = new FeatureImpl("Outlook");
-    featureOutlook.addNewValue("Sunny");
-    featureOutlook.addNewValue("Overcast");
-    featureOutlook.addNewValue("Rain");
-    this.features.addFeature(featureOutlook);
-
-    Feature featureTemperature = new FeatureImpl("Temperature");
-    featureTemperature.addNewValue("Hot");
-    featureTemperature.addNewValue("Mild");
-    featureTemperature.addNewValue("Cool");
-    this.features.addFeature(featureTemperature);
-
-    Feature featureHumidity = new FeatureImpl("Humidity");
-    featureHumidity.addNewValue("High");
-    featureHumidity.addNewValue("Normal");
-    this.features.addFeature(featureHumidity);
-
-    Feature featureWind = new FeatureImpl("Wind");
-    featureWind.addNewValue("Weak");
-    featureWind.addNewValue("Strong");
-    this.features.addFeature(featureWind);
-
-    Instance instance1 = new InstanceImpl();
-    instance1.setExpectedOutput("No");
-    instance1.setNewFeature("Outlook", "Sunny");
-    instance1.setNewFeature("Temperature", "Hot");
-    instance1.setNewFeature("Humidity", "High");
-    instance1.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance1);
-
-    Instance instance2 = new InstanceImpl();
-    instance2.setExpectedOutput("No");
-    instance2.setNewFeature("Outlook", "Sunny");
-    instance2.setNewFeature("Temperature", "Hot");
-    instance2.setNewFeature("Humidity", "High");
-    instance2.setNewFeature("Wind", "Strong");
-    this.instances.addNewInstance(instance2);
-
-    Instance instance3 = new InstanceImpl();
-    instance3.setExpectedOutput("Yes");
-    instance3.setNewFeature("Outlook", "Overcast");
-    instance3.setNewFeature("Temperature", "Hot");
-    instance3.setNewFeature("Humidity", "High");
-    instance3.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance3);
-
-    Instance instance4 = new InstanceImpl();
-    instance4.setExpectedOutput("Yes");
-    instance4.setNewFeature("Outlook", "Rain");
-    instance4.setNewFeature("Temperature", "Mild");
-    instance4.setNewFeature("Humidity", "High");
-    instance4.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance4);
-
-    Instance instance5 = new InstanceImpl();
-    instance5.setExpectedOutput("Yes");
-    instance5.setNewFeature("Outlook", "Rain");
-    instance5.setNewFeature("Temperature", "Cool");
-    instance5.setNewFeature("Humidity", "Normal");
-    instance5.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance5);
-
-    Instance instance6 = new InstanceImpl();
-    instance6.setExpectedOutput("No");
-    instance6.setNewFeature("Outlook", "Rain");
-    instance6.setNewFeature("Temperature", "Cool");
-    instance6.setNewFeature("Humidity", "Normal");
-    instance6.setNewFeature("Wind", "Strong");
-    this.instances.addNewInstance(instance6);
-
-    Instance instance7 = new InstanceImpl();
-    instance7.setExpectedOutput("Yes");
-    instance7.setNewFeature("Outlook", "Overcast");
-    instance7.setNewFeature("Temperature", "Cool");
-    instance7.setNewFeature("Humidity", "Normal");
-    instance7.setNewFeature("Wind", "Strong");
-    this.instances.addNewInstance(instance7);
-
-    Instance instance8 = new InstanceImpl();
-    instance8.setExpectedOutput("No");
-    instance8.setNewFeature("Outlook", "Sunny");
-    instance8.setNewFeature("Temperature", "Mild");
-    instance8.setNewFeature("Humidity", "High");
-    instance8.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance8);
-
-    Instance instance9 = new InstanceImpl();
-    instance9.setExpectedOutput("Yes");
-    instance9.setNewFeature("Outlook", "Sunny");
-    instance9.setNewFeature("Temperature", "Cool");
-    instance9.setNewFeature("Humidity", "Normal");
-    instance9.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance9);
-
-    Instance instance10 = new InstanceImpl();
-    instance10.setExpectedOutput("Yes");
-    instance10.setNewFeature("Outlook", "Rain");
-    instance10.setNewFeature("Temperature", "Mild");
-    instance10.setNewFeature("Humidity", "Normal");
-    instance10.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance10);
-
-    Instance instance11 = new InstanceImpl();
-    instance11.setExpectedOutput("Yes");
-    instance11.setNewFeature("Outlook", "Sunny");
-    instance11.setNewFeature("Temperature", "Mild");
-    instance11.setNewFeature("Humidity", "Normal");
-    instance11.setNewFeature("Wind", "Strong");
-    this.instances.addNewInstance(instance11);
-
-    Instance instance12 = new InstanceImpl();
-    instance12.setExpectedOutput("Yes");
-    instance12.setNewFeature("Outlook", "Overcast");
-    instance12.setNewFeature("Temperature", "Mild");
-    instance12.setNewFeature("Humidity", "High");
-    instance12.setNewFeature("Wind", "Strong");
-    this.instances.addNewInstance(instance12);
-
-    Instance instance13 = new InstanceImpl();
-    instance13.setExpectedOutput("Yes");
-    instance13.setNewFeature("Outlook", "Overcast");
-    instance13.setNewFeature("Temperature", "Hot");
-    instance13.setNewFeature("Humidity", "Normal");
-    instance13.setNewFeature("Wind", "Weak");
-    this.instances.addNewInstance(instance13);
-
-    Instance instance14 = new InstanceImpl();
-    instance14.setExpectedOutput("No");
-    instance14.setNewFeature("Outlook", "Rain");
-    instance14.setNewFeature("Temperature", "Mild");
-    instance14.setNewFeature("Humidity", "High");
-    instance14.setNewFeature("Wind", "Strong");
-    this.instances.addNewInstance(instance14);
-  }
 
   @Test
   public void testTreeInstantiatoin() {
@@ -421,32 +275,200 @@ public class TreeTests {
   }
 
   @Test
-  public void testFullFlow() throws InvalidFeatureValueException {
+  public void testBuildDecisionTree() throws InvalidFeatureValueException {
 
-    Node tree = this.tree.buildDecisionTree(this.features, this.instances);
+    NodeType nodeFeature = NodeType.FEATURE_NODE;
+    NodeType nodeLeaf = NodeType.OUTPUT_LEAF_NODE;
 
-    assertEquals(NodeType.FEATURE_NODE, tree.getNodeType());
-    assertEquals("Outlook", tree.getFeature().getName());
+    String featureOutlook = "Outlook";
+    String firstOutlookValue = "Overcast";
+    String secondOutlookValue = "Sunny";
+    String thirdOutlookValue = "Rain";
 
-    assertEquals(NodeType.OUTPUT_LEAF_NODE, tree.getChildNode("Overcast").getNodeType());
-    assertEquals("Yes", tree.getChildNode("Overcast").getOutput());
+    String featureHumidity = "Humidity";
+    String firstHumidityValue = "High";
+    String secondHumidityValue = "Normal";
 
-    assertEquals(NodeType.FEATURE_NODE, tree.getChildNode("Sunny").getNodeType());
-    assertEquals("Humidity", tree.getChildNode("Sunny").getFeature().getName());
+    String featureWind = "Wind";
+    String firstWindValue = "Strong";
+    String secondWindValue = "Weak";
 
-    assertEquals(NodeType.OUTPUT_LEAF_NODE, tree.getChildNode("Sunny").getChildNode("High").getNodeType());
-    assertEquals("No", tree.getChildNode("Sunny").getChildNode("High").getOutput());
+    String outputPositive = "Yes";
+    String outputNegative = "No";
 
-    assertEquals(NodeType.OUTPUT_LEAF_NODE, tree.getChildNode("Sunny").getChildNode("Normal").getNodeType());
-    assertEquals("Yes", tree.getChildNode("Sunny").getChildNode("Normal").getOutput());
+    Tree id3 = new TreeImpl();
 
-    assertEquals(NodeType.FEATURE_NODE, tree.getChildNode("Rain").getNodeType());
-    assertEquals("Wind", tree.getChildNode("Rain").getFeature().getName());
+    Instances instances = new InstancesImpl();
+    Features features = new FeaturesImpl();
 
-    assertEquals(NodeType.OUTPUT_LEAF_NODE, tree.getChildNode("Rain").getChildNode("Strong").getNodeType());
-    assertEquals("No", tree.getChildNode("Rain").getChildNode("Strong").getOutput());
+    testBuildDecisionTree_PopulateFeatures(features);
+    testBuildDecisionTree_PopulateInstances(instances);
 
-    assertEquals(NodeType.OUTPUT_LEAF_NODE, tree.getChildNode("Rain").getChildNode("Weak").getNodeType());
-    assertEquals("Yes", tree.getChildNode("Rain").getChildNode("Weak").getOutput());
+    Node tree = id3.buildDecisionTree(features, instances);
+
+    assertEquals(nodeFeature, tree.getNodeType());
+    assertEquals(featureOutlook, tree.getFeature().getName());
+
+    assertEquals(nodeLeaf, tree.getChildNode(firstOutlookValue).getNodeType());
+    assertEquals(outputPositive, tree.getChildNode(firstOutlookValue).getOutput());
+
+    assertEquals(nodeFeature, tree.getChildNode(secondOutlookValue).getNodeType());
+    assertEquals(featureHumidity, tree.getChildNode(secondOutlookValue).getFeature().getName());
+
+    assertEquals(nodeLeaf, tree.getChildNode(secondOutlookValue).getChildNode(firstHumidityValue).getNodeType());
+    assertEquals(outputNegative, tree.getChildNode(secondOutlookValue).getChildNode(firstHumidityValue).getOutput());
+
+    assertEquals(nodeLeaf, tree.getChildNode(secondOutlookValue).getChildNode(secondHumidityValue).getNodeType());
+    assertEquals(outputPositive, tree.getChildNode(secondOutlookValue).getChildNode(secondHumidityValue).getOutput());
+
+    assertEquals(nodeFeature, tree.getChildNode(thirdOutlookValue).getNodeType());
+    assertEquals(featureWind, tree.getChildNode(thirdOutlookValue).getFeature().getName());
+
+    assertEquals(nodeLeaf, tree.getChildNode(thirdOutlookValue).getChildNode(firstWindValue).getNodeType());
+    assertEquals(outputNegative, tree.getChildNode(thirdOutlookValue).getChildNode(firstWindValue).getOutput());
+
+    assertEquals(nodeLeaf, tree.getChildNode(thirdOutlookValue).getChildNode(secondWindValue).getNodeType());
+    assertEquals(outputPositive, tree.getChildNode(thirdOutlookValue).getChildNode(secondWindValue).getOutput());
+  }
+
+  private void testBuildDecisionTree_PopulateFeatures(Features features) {
+
+    Feature feature = new FeatureImpl("Outlook");
+    feature.addNewValue("Sunny");
+    feature.addNewValue("Overcast");
+    feature.addNewValue("Rain");
+    features.addFeature(feature);
+
+    feature = new FeatureImpl("Temperature");
+    feature.addNewValue("Hot");
+    feature.addNewValue("Mild");
+    feature.addNewValue("Cool");
+    features.addFeature(feature);
+
+    feature = new FeatureImpl("Humidity");
+    feature.addNewValue("High");
+    feature.addNewValue("Normal");
+    features.addFeature(feature);
+
+    feature = new FeatureImpl("Wind");
+    feature.addNewValue("Weak");
+    feature.addNewValue("Strong");
+    features.addFeature(feature);
+  }
+
+  private void testBuildDecisionTree_PopulateInstances(Instances instances) {
+
+    Instance instance;
+
+    instance = new InstanceImpl();
+    instance.setNewFeature("Outlook", "Sunny");
+    instance.setNewFeature("Temperature", "Hot");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Weak");
+    instance.setExpectedOutput("No");
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("No");
+    instance.setNewFeature("Outlook", "Sunny");
+    instance.setNewFeature("Temperature", "Hot");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Strong");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Overcast");
+    instance.setNewFeature("Temperature", "Hot");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Rain");
+    instance.setNewFeature("Temperature", "Mild");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Rain");
+    instance.setNewFeature("Temperature", "Cool");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("No");
+    instance.setNewFeature("Outlook", "Rain");
+    instance.setNewFeature("Temperature", "Cool");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Strong");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Overcast");
+    instance.setNewFeature("Temperature", "Cool");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Strong");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("No");
+    instance.setNewFeature("Outlook", "Sunny");
+    instance.setNewFeature("Temperature", "Mild");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Sunny");
+    instance.setNewFeature("Temperature", "Cool");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Rain");
+    instance.setNewFeature("Temperature", "Mild");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Sunny");
+    instance.setNewFeature("Temperature", "Mild");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Strong");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Overcast");
+    instance.setNewFeature("Temperature", "Mild");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Strong");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("Yes");
+    instance.setNewFeature("Outlook", "Overcast");
+    instance.setNewFeature("Temperature", "Hot");
+    instance.setNewFeature("Humidity", "Normal");
+    instance.setNewFeature("Wind", "Weak");
+    instances.addNewInstance(instance);
+
+    instance = new InstanceImpl();
+    instance.setExpectedOutput("No");
+    instance.setNewFeature("Outlook", "Rain");
+    instance.setNewFeature("Temperature", "Mild");
+    instance.setNewFeature("Humidity", "High");
+    instance.setNewFeature("Wind", "Strong");
+    instances.addNewInstance(instance);
   }
 }
